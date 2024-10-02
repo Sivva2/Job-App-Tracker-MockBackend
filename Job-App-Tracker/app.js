6ef6c7fd732186758f860e71e2ce14ce0e01aa53
Kevin Abaskaran <shiva94460@gmail.com>
2024-10-02 11:16:58 +0200

    Add vercel config

--- Job-App-Tracker/app.js
+++ Job-App-Tracker/app.js
@@ -1,23 +1,25 @@
-require('dotenv').config();
-const jsonServer = require('json-server');
-const morgan = require('morgan');
+require("dotenv").config();
+const jsonServer = require("json-server");
+const morgan = require("morgan");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router('db.json');
+const fs = require("fs");
+const db = JSON.parse(fs.readFileSync("db.json"));
+const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 const PORT = 4000;
 
 server.use(middlewares);
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-	// Middleware to disable CORS
-	res.header('Access-Control-Allow-Origin', '*');
-	next();
+  // Middleware to disable CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
 });
 server.use(router);
 
 server.listen(PORT, () => {
-	console.log(`JSON Server is running on http://localhost:${PORT}`);
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
